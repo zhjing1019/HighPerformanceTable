862ea701cdb7a024924789a6b487b180bf810429
zhangjing <zhangjing1@imeik.com>
2024-08-20 19:03:40 +0800

    feat：element table

--- packages/element-table/src/components/Layout.vue
+++ packages/element-table/src/components/Layout.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>
-    Layout
+  <div class="layout">
+    <!-- 导航部分 -->
+    <div class=""></div>
+    <!-- 内容部分 -->
   </div>
 </template>
 
 <script>
 export default {
   name: 'LayoutVue',
-  components: {}
+  components: {},
+  data() {
+    return {
+      current: 'ElementTable',
+      layoutList: [
+        {
+          // 普通的element表格
+          name: 'element-table',
+          value: 'ElementTable'
+        }
+      ]
+    };
+  }
 };
 </script>
 
